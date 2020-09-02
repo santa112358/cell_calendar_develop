37d9286ec07d7da6dd7e497ecce3e68bd34936df
santa112358 <sun.jp.0212@gmail.com>
2020-09-03 04:42:22 +0900

    fix height

--- lib/cell_calendar/cell_calendar.dart
+++ lib/cell_calendar/cell_calendar.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-const double _headerHeight = 84;
+const double _monthLabelHeight = 52;
+
+const double _weekDayLabelsHeight = 32;
+
+const double _headerHeight = _monthLabelHeight + _weekDayLabelsHeight;
 
 class CellCalendar extends StatelessWidget {
   Widget _tableCell(double height) {
@@ -10,7 +14,10 @@ class CellCalendar extends StatelessWidget {
       width: double.infinity,
       child: Column(
         children: [
-          Text("d"),
+          const SizedBox(
+            height: 4,
+          ),
+          Text("day"),
         ],
       ),
     );
@@ -36,53 +43,56 @@ class CellCalendar extends StatelessWidget {
     final bodyHeight = box.size.height;
     final tableRowHeight = (bodyHeight - _headerHeight) / 6;
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         SizedBox(
-          height: _headerHeight,
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.arrow_back_ios),
-                    onPressed: () {},
-                  ),
-                  Text("Month"),
-                  IconButton(
-                    icon: Icon(Icons.arrow_forward_ios),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(child: Text("Sun", textAlign: TextAlign.center)),
-                  Expanded(child: Text("Mon", textAlign: TextAlign.center)),
-                  Expanded(child: Text("Tue", textAlign: TextAlign.center)),
-                  Expanded(child: Text("Wed", textAlign: TextAlign.center)),
-                  Expanded(child: Text("Thu", textAlign: TextAlign.center)),
-                  Expanded(child: Text("Fri", textAlign: TextAlign.center)),
-                  Expanded(child: Text("Sat", textAlign: TextAlign.center)),
-                ],
-              )
-            ],
+          height: _monthLabelHeight,
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+            child: Text(
+              "Month",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
           ),
         ),
         Expanded(
           child: PageView.builder(
             itemBuilder: (context, index) {
-              return Table(
-                border: TableBorder.all(
-                    width: 1, color: Theme.of(context).dividerColor),
+              return Column(
                 children: [
-                  _tableRow(tableRowHeight),
-                  _tableRow(tableRowHeight),
-                  _tableRow(tableRowHeight),
-                  _tableRow(tableRowHeight),
-                  _tableRow(tableRowHeight),
-                  _tableRow(tableRowHeight),
+                  SizedBox(
+                    height: _weekDayLabelsHeight,
+                    child: Row(
+                      children: [
+                        Expanded(
+                            child: Text("Sun", textAlign: TextAlign.center)),
+                        Expanded(
+                            child: Text("Mon", textAlign: TextAlign.center)),
+                        Expanded(
+                            child: Text("Tue", textAlign: TextAlign.center)),
+                        Expanded(
+                            child: Text("Wed", textAlign: TextAlign.center)),
+                        Expanded(
+                            child: Text("Thu", textAlign: TextAlign.center)),
+                        Expanded(
+                            child: Text("Fri", textAlign: TextAlign.center)),
+                        Expanded(
+                            child: Text("Sat", textAlign: TextAlign.center)),
+                      ],
+                    ),
+                  ),
+                  Table(
+                    border: TableBorder.all(
+                        width: 1, color: Theme.of(context).dividerColor),
+                    children: [
+                      _tableRow(tableRowHeight),
+                      _tableRow(tableRowHeight),
+                      _tableRow(tableRowHeight),
+                      _tableRow(tableRowHeight),
+                      _tableRow(tableRowHeight),
+                      _tableRow(tableRowHeight),
+                    ],
+                  ),
                 ],
               );
             },
